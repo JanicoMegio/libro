6fcf1b9d4f4515335ee701a155829c43867d6c24
JanicoMegio <janicov.m@gmail.com>
2023-09-27 14:46:13 +0800

    commit

--- store/book/context_processors.py
+++
@@ -1,8 +0,0 @@
-# from book.models import CartItems
-# from django.contrib.auth.decorators import login_required
-
-# @login_required(login_url='login')
-# def cart_count(request):
-# 	user = request.user
-# 	cart_count = CartItems.objects.filter(user_name=user).count()
-# 	return {'cart_count': cart_count}
